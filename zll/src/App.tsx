375403817a454b6910437237a076075f64a72558
lulu-lesly <dearlulu1995@gmail.com>
2021-12-27 19:27:08 +0800

    animate

--- zll/src/App.tsx
+++ zll/src/App.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'animate.css';
 
 function App() {
+  const handleAddAnimate = () => {
+  };
+
   return (
     <div className="App">
-      <p>this is new demo </p>
+      <div className="box" onClick={handleAddAnimate}>
+        <p className={`box-title animate__animated animate__bounce`}>welcome to lesly lulu channel</p>
+      </div>
     </div>
   );
 }
