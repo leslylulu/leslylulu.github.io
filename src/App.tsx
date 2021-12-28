c04f9d259a8222e4c21d6afc76a60e309f12241b
lulu-lesly <dearlulu1995@gmail.com>
2021-12-28 10:30:30 +0800

    actions test

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,8 @@ function App() {
   return (
     <div className="App">
       <div className="box" onClick={handleAddAnimate}>
-        <p className={`box-title animate__animated animate__bounce`}>welcome to lesly lulu channel</p>
+        <div className={`box-title animate__animated animate__bounce`}>welcome to lesly lulu channel</div>
+        <span className={`box-title animate__animated animate__backInUp`}>.test</span>
       </div>
     </div>
   );
