49275caa85fd873f22d1377a425aae768b2f04cd
lululesly <dearlulu1995@gmail.com>
2022-01-17 22:02:44 +0800

    新加图片

--- src/assets/pic/home/index.tsx
+++ src/assets/pic/home/index.tsx
@@ -1,7 +1,19 @@
 import sister  from './sisters.jpg';
 import empty  from './empty.png';
+// @ts-ignore
+import pp1 from './pp1.JPG';
+// @ts-ignore
+import move from './move.JPEG';
+// @ts-ignore
+import camera from './camera.JPG';
+// @ts-ignore
+import music from './music.JPG';
 
 export { 
   sister,
-  empty
+  empty,
+  pp1,
+  move,
+  camera,
+  music,
 };
