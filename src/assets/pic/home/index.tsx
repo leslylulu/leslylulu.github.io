a83468b96fdf191ff2e97351b4a750b1bfd356fe
lululesly <dearlulu1995@gmail.com>
2022-02-13 14:45:57 +0800

    pull

--- src/assets/pic/home/index.tsx
+++ src/assets/pic/home/index.tsx
@@ -1,29 +1,15 @@
 import sister  from './sisters.jpg';
 import empty  from './empty.png';
-<<<<<<< HEAD
-import pp from './pp.jpg';
-import move from './move.jpeg';
-import camera from './camera.jpg';
-import music from './music.jpg';
-=======
 // import pp1 from './pp1.jpg';
 // import move from './move.jpeg';
 // import camera from './camera.jpg';
 // import music from './music.jpg';
->>>>>>> d36892fba57b9dd27e76466f7120bced6822107f
 
 export { 
   sister,
   empty,
-<<<<<<< HEAD
-  pp,
-  move,
-  camera,
-  music,
-=======
   // pp1,
   // move,
   // camera,
   // music,
->>>>>>> d36892fba57b9dd27e76466f7120bced6822107f
 };
