aac9d66df0d48bfa04e22c3c6e2e95a685c889b4
zhanglulu <dearlulu1995@gmail.com>
2022-02-25 18:16:13 +0800

    memo

--- src/pages/front/demo/usememo/index.tsx
+++ src/pages/front/demo/usememo/index.tsx
@@ -1,13 +1,50 @@
+import { useState, memo } from "react"
 
+import './index.less';
+
+const Son = (props: any) => {
+  const { numA, noUseClick } = props;
+  console.log("Son 渲染了");
+  return (
+    <div className="son" >
+      <div>this is son Node：</div>
+      <div onClick={noUseClick} >props numA = {numA}</div>
+    </div>
+  ) 
+};
+
+const WarpperSon = memo(Son);
+
+const Parent = () => {
+
+  const [numA, setNumA] = useState(0);
+  const [numB, setNumB] = useState(10);
+
+  const handleAdd = () => {
+    setNumB(numB + 1);
+  }
+
+  const noUseClick = () => {};
+
+  return (
+    <div className='parent'>
+      <div>this is Parent Node: </div>
+      <div>this is numA: {numA}</div>
+      <div className="parent-num-b" onClick={handleAdd}>this is numB: {numB}(我是一个可点击的)</div>
+      {/* <Son numA={numA} /> */}
+      <WarpperSon noUseClick={noUseClick} numA={numA} />
+    </div>
+  )
+}
 
 const UseMemoHook = () => {
   return (
-    <div className="demo">
-      <h3>useMemo是来解决什么问题的？</h3>
+    <div className="memo">
+      <div>useMemo是来解决什么问题的？</div>
       <div>“减少组件重新渲染时不必要的函数计算”。</div>
+      <Parent/>
     </div>
   )
-
 }
 
 export default UseMemoHook;
